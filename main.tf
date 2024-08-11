4f0372137094ee9ed8eab795d8a26baab5a3d0ab
Marcello Di Benedetto <marcellodb17@gmail.com>
2024-08-11 13:27:03 -0600

    Proxmox VM deployment based on input map

--- main.tf
+++ main.tf
@@ -14,23 +14,24 @@ provider "proxmox" {
   pm_api_token_secret = var.pve_api_token_secret
 }
 
-resource "proxmox_vm_qemu" "pxe-minimal-example" {
-    name = "pxe-minimal-example"
-    clone = "ubu-noble-template"
+resource "proxmox_vm_qemu" "vm" {
+    for_each = var.pve_vms
+    name = each.key
+    clone =  each.value.template
     full_clone = true
-    target_node = "pve05"
+    target_node = each.value.node
     onboot = true
     sockets = 1
-    cores = 4
-    memory = 4096
+    cores = each.value.cores
+    memory = each.value.memory_mb
     scsihw = "virtio-scsi-single"
-    ciuser = "marcello"
+    ciuser = var.pve_cloud_init_user
+    sshkeys = var.pve_cloud_init_ssh_key
     ipconfig0 = "ip=dhcp,ip6=dhcp"
-    sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDeOJZRkcHPmpq3vRia9z17SgtaVrEm8GHkGLdX4+B8FLpvq9RqQaBkAzXsj4b49qpTdwiwlUg4Gtt0+T7B7IHat7HLsu2rGBjHgQ7QKoSVBXVrha8KvOtSFE/sgAupWGVz/CWegH5QKCrx1xgK6nRvUc37hzsJGn/0ghUlE2IoDDj3EJc02XpMKtJGEXsGS+O8rTTUJo7SE0gloYU5Wg+nToxpN73YNKGGs+wh05x2uKY0o+NkJ693smrvuoh5ZG2tYblLy3f4ZUqWtTXRbqzLCnx3txi9rfV5xYPpY/8hCHPyzq4yiuFdSzXzaxnLJD/wn9BjWiGt+3NjZa8w9k6wzn1+KKp5tOZt9tS8bAiRdDsoH2jzUDkqLeRqduuGVtWZAHPltFBVPd9L0zJIZl86I55JX7ABzyo4JWLfqhysJgUKAxgQrjOexsasb2mfr6zNl8jSCQMtpdgy+RVjiwj50c/05TzW7TBO96iX6YUQjfNP9fcB6lxPwW93opa0f1s9GHxt123H7jbexjcNci8f9A2eLVxM0DwGlaMfAs+yK3cCVQA9HZGqjDTSZ5t5XnNaYB5JddDXyO4nuqYmP2m4Fj4KR4uXc2ArJ/2GSyvNj6L9vUECa86eE7kgxDGuH1Bkr4jCz846LSS6Lx2SQR7kPTvH0mN07PYaqJJl/kKShw== marcello,2023-02-16"
 
     disks {
         ide {
-            ide2 {
+            ide0 {
                 cloudinit {
                     storage = "local-lvm"
                 }
@@ -41,12 +42,11 @@ resource "proxmox_vm_qemu" "pxe-minimal-example" {
           scsi0 {
             disk {
                 storage = "local-lvm"
-                size = "50G"
+                size = "${each.value.storage_gb}G"
             }
           }
         }
-
-    } 
+    }
 
     network {
         bridge    = "vmbr0"
--- variables.tf
+++ variables.tf
@@ -11,3 +11,21 @@ variable pve_api_token_secret {
     type = string
     sensitive = true
 }
+
+variable pve_vms {
+    type = map(object({
+        node = string
+        template = string
+        cores = number
+        memory_mb = number
+        storage_gb = number
+    }))
+}
+
+variable pve_cloud_init_user {
+    type = string
+}
+
+variable pve_cloud_init_ssh_key {
+    type = string
+}
