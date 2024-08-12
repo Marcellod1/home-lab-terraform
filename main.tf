67bfaf6a6a57b10363fb72db84db1a3a96ea7d48
Marcello Di Benedetto <marcellodb17@gmail.com>
2024-08-11 21:04:17 -0600

    Custom cloud init vendor snippet to install qemu-guest-agent and reboot after deployment

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.1-rc3"
     }
   }
@@ -9,49 +9,50 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url = "https://${var.pve_hostname}/api2/json"
-  pm_api_token_id = var.pve_api_token_id
+  pm_api_url          = "https://${var.pve_hostname}/api2/json"
+  pm_api_token_id     = var.pve_api_token_id
   pm_api_token_secret = var.pve_api_token_secret
 }
 
 resource "proxmox_vm_qemu" "vm" {
-    for_each = var.pve_vms
-    name = each.key
-    clone =  each.value.template
-    full_clone = true
-    target_node = each.value.node
-    onboot = true
-    sockets = 1
-    cores = each.value.cores
-    memory = each.value.memory_mb
-    scsihw = "virtio-scsi-pci"
-    ciuser = var.pve_cloud_init_user
-    sshkeys = var.pve_cloud_init_ssh_key
-    ipconfig0 = "ip=dhcp,ip6=dhcp"
+  for_each    = var.pve_vms
+  target_node = each.value.node
+  name        = each.key
+  clone       = each.value.template
+  full_clone  = true
+  onboot      = true
+  agent       = 1
+  scsihw      = "virtio-scsi-pci"
+  sockets     = 1
+  cores       = each.value.cores
+  memory      = each.value.memory_mb
+  ciuser      = var.pve_cloud_init_user
+  sshkeys     = var.pve_cloud_init_ssh_key
+  cicustom    = "vendor=local:snippets/vendor.yaml"
+  ipconfig0   = "ip=dhcp"
 
-    disks {
-        ide {
-            ide0 {
-                cloudinit {
-                    storage = "local-lvm"
-                }
-            }
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
         }
-
-        scsi {
-          scsi0 {
-            disk {
-                storage = "local-lvm"
-                size = "${each.value.storage_gb}G"
-            }
-          }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage = "local-lvm"
+          size    = "${each.value.storage_gb}G"
         }
+      }
     }
+  }
 
-    network {
-        bridge    = "vmbr0"
-        firewall  = false
-        link_down = false
-        model     = "virtio"
-    }
+  network {
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
+    model     = "virtio"
+  }
 }
